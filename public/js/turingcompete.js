038bc12f8a29a0b77dcf2e4011190e00aa4fe4b7
Spenser Jones <hello@spenserj.com>
2013-09-24 15:30:41 -0600

    Reload the page on reconnection to the server

--- public/js/turingcompete.js
+++ public/js/turingcompete.js
@@ -1,7 +1,15 @@
 var TCModel = function TCModel() {
+  this.connected = ko.observable(false);
   this.chat = new ChatModel();
   this.authentication = new AuthenticationModel();
 };
 
 var tcKnockout = new TCModel();
 ko.applyBindings(tcKnockout);
+
+socket.on('connect', function() {
+  if (tcKnockout.connected() === true) {
+    location.reload(true);
+  }
+  tcKnockout.connected(true);
+});

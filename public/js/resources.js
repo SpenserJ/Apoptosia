079a952e4011ef5844c83bc62c42186fe2d93818
Spenser Jones <hello@spenserj.com>
2013-09-25 13:50:35 -0600

    Reorgainzing Rendering files

--- public/js/resources.js
+++
@@ -1,6 +0,0 @@
-game.resources = [
-  { name: "tileset-shinygold", type:"image", src: "data/img/map/area01_level_tiles.png" },
-  { name: "pokemon", type: "image", src: "data/img/sprite/pokemon.png" },
-  { name: "area01", type: "tmx", src: "data/map/area01.tmx" },
-  { name: "metatiles16x16", type: "image", src: "data/img/map/metatiles16x16.png" },
-];

4175c026e30690792010993627b9129358aa89e8
Spenser Jones <hello@spenserj.com>
2013-09-25 12:37:37 -0600

    Load existing players and remove players that quit

--- public/js/player.js
+++ public/js/player.js
@@ -1,4 +1,5 @@
-var tcMelonSprites = {};
+var tcMelonSprites = {}, tcMelonSpritesToLoad = [];
+var tcMelonLoaded = false;
 
 // Configure the keybindings
 me.input.bindKey(me.input.KEY.A, "left", true);
@@ -6,15 +7,51 @@ me.input.bindKey(me.input.KEY.D, "right", true);
 me.input.bindKey(me.input.KEY.W, "up", true);
 me.input.bindKey(me.input.KEY.S, "down", true);
 
+me.event.subscribe(me.event.LOADER_COMPLETE, function(){
+  setTimeout(checkForMelonEntityReady, 1000);
+});
+
+function checkForMelonEntityReady() {
+  if (typeof me.game.PlayerEntity === 'false') {
+    return setTimeout(checkForMelonEntityReady, 1000);
+  }
+  tcMelonLoaded = true;
+  var loadedSprites = false;
+  while (tcMelonSpritesToLoad.length > 0) {
+    var spriteData = tcMelonSpritesToLoad.shift();
+    tcMelonSprites[spriteData.username] =
+      me.entityPool.newInstanceOf('npc', spriteData.x, spriteData.y, spriteData.pokeID);
+    me.game.add(tcMelonSprites[spriteData.username], 1001);
+    loadedSprites = true;
+  }
+  if (loadedSprites === true) {
+    me.game.sort();
+  }
+}
+
 socket.on('game.player.joined', function(data) {
-  if (typeof tcMelonSprites[data.username] !== 'undefined') {
+  if (typeof tcMelonSprites[data.username] !== 'undefined' ||
+      typeof tcMelonSpritesToLoad[data.username] !== 'undefined') {
     throw 'Duplicate player name!';
   }
 
+  if (tcMelonLoaded === false) {
+    return tcMelonSpritesToLoad.push(data);
+  }
   var mode = (data.username === tcKnockout.authentication.username()) ? 'player': 'npc';
   tcMelonSprites[data.username] = me.entityPool.newInstanceOf(mode, 64, 64, data.pokeID);
-  console.log(tcMelonSprites);
   me.game.add(tcMelonSprites[data.username], 1001);
+  me.game.sort();
+});
+
+socket.on('game.player.left', function(data) {
+  if (typeof tcMelonSprites[data.username] !== 'undefined') {
+    me.game.remove(tcMelonSprites[data.username]);
+    delete tcMelonSprites[data.username];
+  } else if (typeof tcMelonSpritesToLoad[data.username] !== 'undefined') {
+    me.game.remove(tcMelonSpritesToLoad[data.username]);
+    delete tcMelonSpritesToLoad[data.username];
+  }
   me.game.sort();
 });
 
--- server/server.js
+++ server/server.js
@@ -1,28 +1,41 @@
-var usernames = [];
+var players = {};
 
 TC.on('connection', function (socket) {
   socket.emit('version', { version: '0.0.1' });
+  var keys = Object.keys(players), i;
+  for (i = 0; i < keys.length; i++) {
+    var sendPlayer = players[keys[i]];
+    socket.emit('game.player.joined', {
+      username: sendPlayer.username,
+      pokeID: sendPlayer.pokeID,
+      x: sendPlayer.x,
+      y: sendPlayer.y 
+    });
+  }
 });
 
 TC.on('disconnect', function (socket) {
   if (typeof socket.username !== 'undefined' &&
-      usernames.indexOf(socket.username) !== -1) {
-    usernames.splice(usernames.indexOf(socket.username), 1);
+      typeof players[socket.username] !== 'undefined') {
+    delete players[socket.username];
     TC.broadcast('chat.member.left', { username: socket.username });
+    TC.broadcast('game.player.left', { username: socket.username });
   }
 });
 
 TC.on('login', function (socket, data) {
-  if (usernames.indexOf(data.username) !== -1) {
+  if (typeof players[data.username] !== 'undefined') {
     return socket.emit('login.failed', { error: 'Username already in use' });
   }
 
-  usernames.push(data.username);
   socket.username = data.username;
   socket.pokeID = Math.floor(Math.random() * 155 + 1);
+  socket.x = 64;
+  socket.y = 64;
+  players[socket.username] = socket;
   socket.emit('login.successful');
-  TC.broadcast('chat.member.joined', { username: data.username });
-  TC.broadcast('game.player.joined', { username: data.username, pokeID: socket.pokeID });
+  TC.broadcast('chat.member.joined', { username: socket.username });
+  TC.broadcast('game.player.joined', { username: socket.username, pokeID: socket.pokeID, x: 64, y: 64 });
 });
 
 TC.on('chat', function (socket, data) {
@@ -30,5 +43,7 @@ TC.on('chat', function (socket, data) {
 });
 
 TC.on('game.move', function (socket, data) {
-  TC.broadcast('game.player.move', { username: socket.username, x: data.x, y: data.y });
+  socket.x = data.x;
+  socket.y = data.y;
+  TC.broadcast('game.player.move', { username: socket.username, x: socket.x, y: data.y });
 });
